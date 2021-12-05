44802bb2f071a27b7d7ce846614619f2c03f1ce5
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2021-12-04 20:51:57 -0800

    day 2 part 1 solution

---
+++ day02/day02_solution.py
@@ -0,0 +1,41 @@
+
+# submarine 
+# wowee
+
+class Solver:
+
+    def __init__(self,fname='input'):
+        with open('input', 'r') as f:
+            self.lines = f.readlines()
+        self.x = 0
+        self.y = 0
+        self.map = {'forward': self.forward,
+                    'up': self.up,
+                    'down': self.down
+                    }
+    #
+    
+    def forward(self,mag):
+        self.x += int(mag)
+    def up(self,mag):
+        self.y -= int(mag)
+    def down(self,mag):
+        self.y += int(mag)
+    
+    def parse_one(self,line):
+        dir,mag = line.split(' ')
+        self.__getattribute__(dir)(mag)
+    def parse_all(self):
+        for line in self.lines:
+            self.parse_one(line)
+    def broadcast_position(self):
+        print("Current position: (%5i, %5i)"%(self.x,self.y))
+    def coordinate_product(self):
+        print(self.x*self.y)
+#
+
+if __name__=="__main__":
+    solver = Solver()
+    solver.parse_all()
+    solver.broadcast_position()
+    solver.coordinate_product()
