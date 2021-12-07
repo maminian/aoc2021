f44a78fe5b789f3c2af551567dc9a5a91700307c
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2021-12-06 22:52:10 -0800

    day 7 part 2 solution

--- day07/day07_solution.py
+++ day07/day07_solution.py
@@ -19,3 +19,40 @@ fuel_cost = sum( np.abs(positions - optimal_location) )
 
 print("Optimal position: %i"%optimal_location)
 print("Total fuel cost: %i"%fuel_cost)
+
+# part 2.
+
+# fuel cost is now scaling by distance traveled; 
+# each additional displacement is increasing the effort by 
+# one. So, one unit is +1, two units +2, three +3, etc.
+#
+# Metric is more like the sum from 0 to abs(x-xstar) of i, 
+# which is N*(N+1)/2. In plain terms, this is the cost function 
+# which is the average of the mean and median. But it's not clear 
+# that the minimizer here is actually the average of the 
+# argminimizers (the mean and median). Worth a shot, though...
+
+def cost(x,xstar):
+    if isinstance(x,int) or isinstance(x,float) or isinstance(x,np.int64):
+        N = abs(x-xstar)
+        return int( N*(N+1)/2 )
+    else:
+        return sum([cost(xi,xstar) for xi in x])
+# 
+
+mean_location = np.mean(positions)
+median_location = np.median(positions)
+
+av_mm = (mean_location + median_location)/2
+if av_mm != int(av_mm):
+    print("oh no...")
+    print(av_mm)
+    print("Rounding down to int.")
+    av_mm = int(av_mm)
+
+# brute force check values between mean and median.
+candidates = np.arange(median_location,mean_location+1, dtype=int)
+vals = np.array([cost(positions, c) for c in candidates])
+
+for c,v in zip(candidates, vals):
+    print("%10i | %10i"%(c,v))
